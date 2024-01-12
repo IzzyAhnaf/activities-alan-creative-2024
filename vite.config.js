87ca53d073720ac3078987f65657d66116d8a344
Biji Telaso <izzyahnaf695@gmail.com>
2024-01-12 23:58:50 +0700

    commit

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,16 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   base: process.env.NODE_ENV === 'production' ? '/activities-alan-creative-2024/' : '',
+  server: {
+    proxy: {
+      '/activities-alan-creative-2024': {
+        target: 'http://localhost:5000',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/activities-alan-creative-2024/, ''),
+      },
+    }
+  }
+
   // server: {
   //   proxy: {
   //     '/api': {
